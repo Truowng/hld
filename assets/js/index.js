c7285cd10238cc0f485c803a7e065ea03d356fd6
Truowng <quockhanhtruong166@gmail.com>
2024-06-27 00:20:16 +0700

    about pc

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
+const menu = document.querySelector("header .header-nav");
+
 AOS.init();
+
+const openMenu = () => {
+  menu.classList.add("active");
+};
+
+const closeMenu = () => {
+  menu.classList.remove("active");
+};
